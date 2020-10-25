70433046cf0df6897c5fbe2e34d153e3b99c1afe
Hai Mi <haimi.bui.ok@gmail.com>
2020-10-25 12:02:40 +0100

    Add deleting a note function

--- app/src/main/java/com/example/recipe_recorder/MainActivity.java
+++ app/src/main/java/com/example/recipe_recorder/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.recipe_recorder;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -65,6 +67,31 @@ public class MainActivity extends AppCompatActivity
              Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
              intent.putExtra("noteID", position); //to tell us which row of listView was tapped
              startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id)
+            {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Delete?")
+                        .setMessage("Are you sure you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                notes.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+
             }
         });
     }
